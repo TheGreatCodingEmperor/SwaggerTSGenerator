c82c7fb8183a267f9c8ab2ea4b2fd281b25bf678
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2023-07-27 06:05:32 +0800

    1. html 改版

--- ClientApp/src/app/pages/demo/order/order.component.js
+++ ClientApp/src/app/pages/demo/order/order.component.js
@@ -1,71 +1,4 @@
-function div(innerHTMLs, attrs) {
-    return `<div ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</div>`
-}
-
-function span(innerHTMLs, attrs) {
-    return `<span ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</span>`
-}
-
-function p(innerHTMLs, attrs) {
-    return `<p ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</p>`
-}
-
-function table(innerHTMLs, attrs) {
-    return `<table ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</table>`
-}
-
-function tr(innerHTMLs, attrs) {
-    return `<tr ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</tr>`
-}
-
-function th(innerHTMLs, attrs) {
-    return `<th ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</th>`
-}
-
-function td(innerHTMLs, attrs) {
-    return `<td ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</td>`
-}
-
-function button(innerHTMLs, attrs) {
-    return `<button ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</button>`
-}
-
-function form(innerHTMLs, attrs) {
-    return `<form ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</form>`
-}
-
-function input(attrs) {
-    return `<input ${attributeStr(attrs)}>`
-}
-function pButton(innerHTMLs, attrs) {
-    if(!attrs){
-        attrs = {};
-    }
-    attrs.class = attrs.class? attrs.class+='p-button-outlined':'p-button-outlined';
-    return `<button pButton ${attributeStr(attrs)}>${innerHTMLs.join('\r\n')}</button>`
-}
-function pInput(attrs) {
-    if(!attrs){
-        attrs = {};
-    }
-    return `<input pInputText ${attributeStr(attrs)}>`
-}
-
-function attributeStr(attrs) {
-    return Object.keys(attrs).map(key => {
-        switch (key) {
-            case 'directive':
-            case 'd': {
-                return attrs[key];
-            }
-            default: {
-                return `${key}="${attrs[key]}"`;
-            }
-        }
-    }).join(' ');
-}
-
-let html = div([
+div([
     span(['Name'], {
         class: 'p-inputgroup-addon'
     }),
@@ -79,4 +12,3 @@ let html = div([
 ], {
     class: 'p-inputgroup'
 });
-console.log(html);
